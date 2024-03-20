97b660ef44bc7b4418cad9dece59bc5cbdc3142f
Pratap Kotala <pratap.kotala@ndsu.edu>
2024-03-20 10:55:19 -0500

    Added employee_detail screen and delete employee record operation

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Employee
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'website/home.html', {})
+    employee_list = Employee.objects.all()
+    return render(request, 'website/home.html', {'emp_list' : employee_list})
 
 def login_user(request):
     if request.method == "POST":
@@ -27,4 +29,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out successfully.')
     return redirect('home')
+
+def employee_view(request,pk):
+    if request.user.is_authenticated:
+        current_employee = Employee.objects.get(pk=pk)
+        return render(request, 'website/employee.html', {'emp' : current_employee})
+    else:
+        return redirect('home')
+    
+def delete(request, pk):
+    if request.user.is_authenticated:
+        current_employee = Employee.objects.get(pk=pk)
+        current_employee.delete()
+        messages.success(request, 'Employee record deleted successfully')
+        return redirect('home')
+    else:
+        messages.success(request, 'You have to be logged in to delete a record')
+        return redirect('home') 
 
